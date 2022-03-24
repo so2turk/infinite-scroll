3d21b86c11964f7793136f6866b4032cad0b4d95
Serhat Ozturk <so2turk@yahoo.com>
2022-03-24 19:40:52 +0100

    Add nodemon as dev dependency

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,32 @@ import { ApolloServer, gql } from 'apollo-server-express'
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String,
+    hifive: Int,
+    hi4: String,
+    user: [User!]!
+  }
+  type User {
+    name: String!,
+    email: String!,
+    age: Int!,
+    happy: Boolean!
   }
 `
 
 const resolvers = {
   Query: {
     hello: () => 'Hi there!',
+    hifive: () => 5,
+    hi4: () => 'four',
+    user: () => {
+      return [{
+        name: 'Lol',
+        email: 'l@l.com',
+        age: 32,
+        happy: true
+      }]
+    }
   },
 }
 const app = express()
