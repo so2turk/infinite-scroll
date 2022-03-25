9ef83bd46a7cbddff1de838a4585e734b6dafedb
Serhat Ozturk <so2turk@yahoo.com>
2022-03-25 12:17:53 +0100

    Refactor index file according to new typeDefs and reselovers

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,24 @@
 import express from 'express'
-import { ApolloServer, gql } from 'apollo-server-express'
+import { ApolloServer } from 'apollo-server-express'
+import typeDefs from './models/schemaRU.js'
+import resolvers from './models/resolverRU.js'
 
-const typeDefs = gql`
-  type Query {
-    hello: String,
-    hifive: Int,
-    hi4: String,
-    user: [User!]!
-  }
-  type User {
-    name: String!,
-    email: String!,
-    age: Int!,
-    happy: Boolean!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hi there!',
-    hifive: () => 5,
-    hi4: () => 'four',
-    user: () => {
-      return [{
-        name: 'Lol',
-        email: 'l@l.com',
-        age: 32,
-        happy: true
-      }]
-    }
-  },
-}
 const app = express()
 
-const server = new ApolloServer({ typeDefs, resolvers })
-await server.start()
-server.applyMiddleware({ app })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
-const port = process.env.PORT || 3000
+await server.start()
+server.applyMiddleware({ app, path: '/api' })
+
+app.get('/', (req, res) => res.send('Server is up and running'))
+app.use((req, res, next) => {
+  res.status(404).send('not found');
+})
+
+const port = process.env.PORT || 5050
 app.listen(port, () => {
   console.log(`Listening on port ${port}${server.graphqlPath}`)
 })
