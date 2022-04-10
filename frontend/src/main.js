8b58fd7f61e6c439c1cc537661dfac2704fd1ad0
Serhat Ozturk <so2turk@yahoo.com>
2022-04-10 12:06:26 +0200

    Implement apollo-client to vue2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,25 @@ import App from './app.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:5050/api'
+})
+  
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
